71fac65e1f4ef38e199e903c457048ad5773d981
goranpavicevic <goran.pavicevic.96@gmail.com>
2018-11-09 03:16:10 +0100

    Certificate and digital signature

--- CentralizedLoggingService/CLS/Program.cs
+++ CentralizedLoggingService/CLS/Program.cs
@@ -1,8 +1,12 @@
 ﻿using Common;
+using Manager;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Principal;
 using System.ServiceModel;
+using System.ServiceModel.Security;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,7 +18,16 @@ namespace CLS
 
         static void Main(string[] args)
         {
-         
+
+            string srvCertCN = Formatter.ParseName(WindowsIdentity.GetCurrent().Name);
+            host.Credentials.ClientCertificate.Authentication.CertificateValidationMode = X509CertificateValidationMode.Custom;
+            host.Credentials.ClientCertificate.Authentication.CustomCertificateValidator = new ServiceCertValidator();
+
+            ///If CA doesn't have a CRL associated, WCF blocks every client because it cannot be validated
+            host.Credentials.ClientCertificate.Authentication.RevocationMode = X509RevocationMode.NoCheck;
+
+            ///Set appropriate service's certificate on the host. Use CertManager class to obtain the certificate based on the "srvCertCN"
+            host.Credentials.ServiceCertificate.Certificate = CertManager.GetCertificateFromStorage(StoreName.My, StoreLocation.LocalMachine, srvCertCN);
             do
             {
                 try
@@ -41,8 +54,12 @@ namespace CLS
 
         public static ServiceHost HostServices()
         {
+            string srvCertCN = Formatter.ParseName(WindowsIdentity.GetCurrent().Name);
+
             NetTcpBinding binding = new NetTcpBinding();
             string address = "net.tcp://localhost:12005/CLS";
+
+            binding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Certificate;
 
             ServiceHost host = new ServiceHost(typeof(Logger));
             host.AddServiceEndpoint(typeof(ILogger), binding, address);
